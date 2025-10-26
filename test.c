f5683df4c0acafa8d04ce772cb5431261d38d088
cctv130 <chinaxke@gmail.com>
2025-10-26 17:25:27 +0800

    Update test.c

--- test.c
+++ test.c
@@ -27,7 +27,19 @@
 #include "win32.h"
 #ifdef _WIN32
 #include <windows.h>
-#define usleep(x) Sleep((x) / 1000) 
+
+void usleep(__int64 usec) 
+{ 
+    HANDLE timer; 
+    LARGE_INTEGER ft; 
+
+    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+    timer = CreateWaitableTimer(NULL, TRUE, NULL); 
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0); 
+    WaitForSingleObject(timer, INFINITE); 
+    CloseHandle(timer); 
+}
 #else
 #include <unistd.h>
 #endif
